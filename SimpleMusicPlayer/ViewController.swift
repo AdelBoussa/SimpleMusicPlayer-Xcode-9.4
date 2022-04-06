a0114473c9cbfc090aa6879c3c096996b17735e6
Apple <administrator@Administrators-iMac-6.local>
2022-04-05 22:16:48 -0700

    first commit

--- SimpleMusicPlayer/ViewController.swift
+++ SimpleMusicPlayer/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func soundFiles() {
+        let bundle = Bundle.main
+        guard let sound = bundle.path(forResource: "DownMaster1", ofType: "mp3") else { return }
+        do {
+            print("sound found\n")
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+        } catch {
+            print("sound not found\n")
+        }
     }
+    @IBAction func p(_ sender: Any) {
+        soundFiles()
+        audioPlayer.play()
 
-
+    }
+    @IBAction func paus(_ sender: Any) {
+        audioPlayer.pause()
+    }
+    //  @IBAction func pb(_ sender: UIButton) {
+    //    soundFiles()
+      //  audioPlayer.play()
+   // }
+    
+    
 }
-
